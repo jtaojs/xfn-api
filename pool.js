67401a013f2835ab7af4cea2fe5530f5fd82feca
jtaojs <15094061928@163.com>
2019-01-28 19:07:53 +0800

    修改连接池

--- pool.js
+++ pool.js
@@ -5,7 +5,7 @@ var pool=mysql.createPool({
   port: '3306',       //数据库端口
   user: 'root',       //数据库管理员
   password: '',       //数据库密码
-  database: 'xfn',     //数据库名称
+  database: 'xiaofeiniu',     //数据库名称
   connectionLimit: 15   //链接池中链接数量
 });
 //将来要被其它的模块使用
